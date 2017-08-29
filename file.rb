5074c443d23d91993c195091db7bbd8549bebebf
Radosław Bułat <radek.bulat@gmail.com>
2017-08-29 14:02:41 +0200

    Call all say_* methods in say_everything

--- file.rb
+++ file.rb
@@ -20,7 +20,11 @@ class Hello
   end
 end
 
-hello = Hello.new
-hello.say_hello
-hello.say_goodbye
-hello.say_hi
+def say_everything
+  hello = Hello.new
+  hello.say_hello
+  hello.say_goodbye
+  hello.say_hi
+end
+
+say_everything
